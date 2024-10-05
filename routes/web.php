cc62f8b0010b18c5f58cb2d067f4cdbf34b84bca
LyenZo <al222310597@gmail.com>
2024-10-04 21:21:12 -0600

    ejercicio de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//la variable con un simbolo de "?" significa de que no necesariamente debe llenarse
+Route::get('/principal',function(){
+    return "Esta va a ser la pagina principal";
+});
+Route::get('/cursos/{curso?}/{categoria?}',function($curso=null,$categoria=null){
+    
+    if($curso && $categoria){
+        return "Este es un ejemplo de curso de nombre {$curso} con la categoria {$categoria}";
+    }
+
+    if($curso){
+        return "Esta es un ejemplo de curso de nombre {$curso}";
+    }
+
+    return "Esta va a ser la pagina donde se mostraran todos los cursos";
+
+});
